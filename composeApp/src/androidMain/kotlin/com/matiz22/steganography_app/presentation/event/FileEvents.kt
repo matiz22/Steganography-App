6d359e15bf38a92fb7c8316a205d40a9218ec1b8
matiz22 <mateuszmalich@gmail.com>
2024-04-11 20:04:09 +0200

    moving from Uri to Photo

--- composeApp/src/androidMain/kotlin/com/matiz22/steganography_app/presentation/event/FileEvents.kt
+++ composeApp/src/androidMain/kotlin/com/matiz22/steganography_app/presentation/event/FileEvents.kt
@@ -1,11 +1,11 @@
 package com.matiz22.steganography_app.presentation.event
 
-import android.net.Uri
+import domain.model.PhotoType
 
 
 sealed class FileEvents {
-    data class PickFile(val uri: Uri?) : FileEvents()
+    data class PickFile(val bytes: ByteArray, val type: PhotoType) : FileEvents()
     data object UnpickFile : FileEvents()
     data class UpdateMessage(val message: String) : FileEvents()
-    data object AddMessage: FileEvents()
+    data object AddMessage : FileEvents()
 }
