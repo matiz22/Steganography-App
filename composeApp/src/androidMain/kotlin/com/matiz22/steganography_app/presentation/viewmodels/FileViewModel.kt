dee8559a7c8bf2fa6cac8183aef9b8e6884ebb00
matiz22 <mateuszmalich@gmail.com>
2024-04-14 17:41:28 +0200

    new icons, error handling

--- composeApp/src/androidMain/kotlin/com/matiz22/steganography_app/presentation/viewmodels/FileViewModel.kt
+++ composeApp/src/androidMain/kotlin/com/matiz22/steganography_app/presentation/viewmodels/FileViewModel.kt
@@ -2,6 +2,7 @@ package com.matiz22.steganography_app.presentation.viewmodels
 
 import FileProcessingRepositoryImpl
 import android.net.Uri
+import android.os.Environment
 import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -13,7 +14,14 @@ import com.matiz22.steganography_app.presentation.event.FileEvents
 import domain.model.Photo
 import domain.model.PhotoType
 import domain.repository.FileProcessingRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import util.resolveImageExtension
+import java.io.File
+import java.io.FileOutputStream
 
 class FileViewModel : ViewModel() {
     var selectedImage by mutableStateOf<Photo?>(null)
@@ -21,15 +29,19 @@ class FileViewModel : ViewModel() {
     var message by mutableStateOf<String>("")
 
     private val fileProcessingRepository: FileProcessingRepository = FileProcessingRepositoryImpl()
-
+    private val _resultsChannel = Channel<String>()
+    val resultsChannel = _resultsChannel.receiveAsFlow()
     fun onEvent(fileEvent: FileEvents) {
         when (fileEvent) {
             is FileEvents.PickFile -> {
                 selectedImage = Photo(
-                    message = "",
-                    photo = fileEvent.bytes,
-                    photoType = PhotoType.JPG
+                    message = "", photo = fileEvent.bytes, photoType = fileEvent.type
                 )
+                viewModelScope.launch {
+                    val message = fileProcessingRepository.readMessage(photo = selectedImage!!)
+                    selectedImage = selectedImage?.copy(message = message!!)
+                }
+
             }
 
             is FileEvents.UnpickFile -> {
@@ -41,10 +53,42 @@ class FileViewModel : ViewModel() {
             }
 
             is FileEvents.AddMessage -> {
-                if (selectedImage != null){
+                if (selectedImage != null) {
                     val photo = selectedImage!!.copy(message = message)
                     viewModelScope.launch {
                         selectedImage = fileProcessingRepository.addMessage(photo = photo)
+                        if (selectedImage == null) {
+                            _resultsChannel.send("File not supported")
+                        } else {
+                            _resultsChannel.send("Message added")
+                        }
+                    }
+                }
+            }
+
+            is FileEvents.SaveToStorage -> {
+                if (selectedImage != null) {
+                    viewModelScope.launch {
+                        withContext(Dispatchers.IO) {
+                            val file = File(
+                                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                                "${System.currentTimeMillis()}.${selectedImage?.photoType?.name}"
+                            )
+                            FileOutputStream(file).use {
+                                it.write(selectedImage?.photo)
+                            }
+                        }
+                        withContext(Dispatchers.Main) {
+                            _resultsChannel.send("File saved to picture folder")
+                        }
+                    }
+
+
+                } else {
+                    viewModelScope.launch {
+                        withContext(Dispatchers.Main) {
+                            _resultsChannel.send("File not selected")
+                        }
                     }
                 }
             }
--- shared/src/commonMain/kotlin/domain/repository/FilesRepository.kt
+++
@@ -1,7 +0,0 @@
-package domain.repository
-
-import domain.model.Photo
-
-interface FilesRepository {
-    suspend fun savePhoto(photo: Photo): Boolean
-}
